8946cadb03a757c7ea834caf50dc394971012424
ganiluffy <ganesh555gari@gmail.com>
2026-01-21 01:18:28 +0530

    backend jwt and mongodb is done

--- backend-node/index.js
+++ backend-node/index.js
@@ -4,14 +4,32 @@ require("dotenv").config();
 
 const app = express();
 
+const connectDB = require("./config/db");
+
+// Connect DB
+connectDB();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+
+
+
+
 app.get("/", (req, res) => {
   res.send("Taxi Agency Backend is running");
+});
+const auth = require("./middleware/authMiddleware");
+
+app.get("/api/protected", auth, (req, res) => {
+  res.json({ message: "Protected data", user: req.user });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+console.log(process.env.JWT_SECRET);
